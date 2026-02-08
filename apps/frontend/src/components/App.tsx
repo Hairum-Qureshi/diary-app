741b1189091448c112d1092dc27578bbaffb61d8
Hairum Qureshi <hqureshi@udel.edu>
2026-02-08 02:24:27 -0500

    added new route for posting diary entries

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -6,6 +6,7 @@ import Dashboard from "../pages/Dashboard";
 import "../css/index.css";
 import Navbar from "./Navbar";
 import Calendar from "../pages/Calendar";
+import DiaryForm from "../pages/DiaryForm";
 
 export default function App() {
 	return (
@@ -16,6 +17,7 @@ export default function App() {
 				<Route path="/about" element={<About />} />
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/calendar" element={<Calendar />} />
+				<Route path="/new-entry" element={<DiaryForm />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
