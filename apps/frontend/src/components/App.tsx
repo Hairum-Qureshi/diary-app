1bb4bdf43bee427affe208e4bd7f3e154fa9fdce
Hairum Qureshi <hqureshi@udel.edu>
2026-02-08 02:20:41 -0500

    added a calendar route

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -4,14 +4,18 @@ import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Dashboard from "../pages/Dashboard";
 import "../css/index.css";
+import Navbar from "./Navbar";
+import Calendar from "../pages/Calendar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/calendar" element={<Calendar />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
