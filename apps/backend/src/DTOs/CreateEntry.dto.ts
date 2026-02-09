d2c2f72a3428ff7d480875f74ff21f2f3eb14e8b
Hairum Qureshi <hqureshi@udel.edu>
2026-02-09 08:48:43 -0500

    added 'date' to CreateEntry DTO

---
+++ apps/backend/src/DTOs/CreateEntry.dto.ts
@@ -0,0 +1,18 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateEntry {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsEnum(['public', 'private'])
+  visibility: 'public' | 'private';
+}
