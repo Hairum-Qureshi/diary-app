6a19e02f0bf4ba9950bccf4927a78f7332f13a28
Hairum Qureshi <hqureshi@udel.edu>
2026-02-09 13:34:19 -0500

    removed 'createdAt' from being set to the date the user sets

--- apps/backend/src/entry/entry.service.ts
+++ apps/backend/src/entry/entry.service.ts
@@ -10,25 +10,25 @@ export class EntryService {
     @InjectModel(Entry.name) private entryModel: Model<EntryDocument>,
   ) {}
 
+  private getStartAndEndOfDayUTC(date: Date) {
+    const startOfDay = new Date(date);
+    startOfDay.setUTCHours(0, 0, 0, 0);
+
+    const endOfDay = new Date(date);
+    endOfDay.setUTCHours(23, 59, 59, 999);
+
+    return { startOfDay, endOfDay };
+  }
+
   async createEntry(createEntryDto: CreateEntry, uid: string) {
     const date = new Date(createEntryDto.date);
 
-    const startOfday = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-    );
-
-    const endOfDay = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate() + 1,
-    );
+    const { startOfDay, endOfDay } = this.getStartAndEndOfDayUTC(date);
 
     const entryExists = await this.entryModel.findOne({
       uid,
       createdAt: {
-        $gte: startOfday,
+        $gte: startOfDay,
         $lt: endOfDay,
       },
     });
@@ -38,9 +38,28 @@ export class EntryService {
     const createdEntry = new this.entryModel({
       ...createEntryDto,
       uid,
-      createdAt: date,
     });
 
     return createdEntry.save();
+  }
+
+  async getEntryByDate(month: string, day: string, year: string, uid: string) {
+    const date = new Date(
+      Date.UTC(Number(year), Number(month) - 1, Number(day)),
+    );
+
+    const { startOfDay, endOfDay } = this.getStartAndEndOfDayUTC(date);
+
+    return (
+      (await this.entryModel
+        .findOne({
+          uid,
+          createdAt: {
+            $gte: startOfDay,
+            $lt: endOfDay,
+          },
+        })
+        .select('-__v')) || []
+    );
   }
 }
