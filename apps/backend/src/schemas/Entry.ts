768e9cc810047d10a48dc620d813d0d45a7b0bfe
Hairum Qureshi <hqureshi@udel.edu>
2026-02-08 23:54:47 -0500

    created 'Entry' schema

---
+++ apps/backend/src/schemas/Entry.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class Entry {
+  @Prop({ type: String })
+  uid: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: 'private' })
+  visibility: 'public' | 'private';
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+SchemaFactory.createForClass(Entry);
+export const EntrySchema = SchemaFactory.createForClass(Entry);
+export type EntryDocument = HydratedDocument<Entry>;
