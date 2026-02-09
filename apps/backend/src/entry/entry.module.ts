3c11ca517555bcae645a7696ea5f5d701dbb3c49
Hairum Qureshi <hqureshi@udel.edu>
2026-02-09 09:10:10 -0500

    added 'AuthModule' to imports array

---
+++ apps/backend/src/entry/entry.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { EntryService } from './entry.service';
+import { EntryController } from './entry.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Entry, EntrySchema } from 'src/schemas/Entry';
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  providers: [EntryService],
+  controllers: [EntryController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Entry.name,
+        schema: EntrySchema,
+      },
+    ]),
+    AuthModule,
+  ],
+})
+export class EntryModule {}
