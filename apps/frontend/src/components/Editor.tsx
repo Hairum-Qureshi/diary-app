32ea16ef0e1627daa8a970f92e01b3fa3c369ec4
Hairum Qureshi <hqureshi@udel.edu>
2026-02-09 01:41:22 -0500

    configured TipTap editor

---
+++ apps/frontend/src/components/Editor.tsx
@@ -0,0 +1,46 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Link from "@tiptap/extension-link"; // only add this if you want link functionality
+import Underline from "@tiptap/extension-underline"; // only add this if you want underline functionality
+import Toolbar from "./Toolbar";
+import "../css/index.css";
+import { Placeholder } from "@tiptap/extensions";
+
+export default function Editor() {
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			Link,
+			Underline,
+			Placeholder.configure({ placeholder: "Write something..." })
+		],
+		editorProps: {
+			attributes: {
+				class: "focus:outline-none" // hides the default outline when clicking in the editor
+			}
+		},
+		enablePasteRules: true, // disable Markdown when pasting
+		enableInputRules: true // disable Markdown when typing
+	});
+
+	if (!editor) return null;
+
+	return (
+		<div className="w-full">
+			<Toolbar editor={editor} />
+			<div
+				className="text-white mx-4 mt-3 leading-5 h-72 overflow-y-auto
+                [&_h1]:text-2xl [&_h1]:font-bold [&_h1]:mb-4
+                [&_h3]:text-xl [&_h3]:font-bold [&_h3]:mb-4
+                [&_p]:mb-2
+                [&_blockquote]:pl-4 [&_blockquote]:border-l-4 [&_blockquote]:border-sky-500 [&_blockquote]:italic [&_blockquote]:text-slate-400
+                [&_ul]:list-disc [&_ul]:pl-6
+                [&_ol]:list-decimal [&_ol]:pl-6
+                [&_li]:mb-1
+                [&_a]:text-sky-400 [&_a]:underline [&_a]:hover:text-sky-300 border-b border-green-900/60 pb-4"
+			>
+				<EditorContent editor={editor} />
+			</div>
+		</div>
+	);
+}
