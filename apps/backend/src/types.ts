68245bd201dc62d48bd9b18c00b2697202973514
Hairum Qureshi <hqureshi@udel.edu>
2026-02-11 10:51:24 -0500

    created 'EntriesArchive' type

--- apps/backend/src/types.ts
+++ apps/backend/src/types.ts
@@ -20,6 +20,11 @@ type AuthRequest = Request & {
   user?: UserPayload;
 };
 
+type EntriesArchive = {
+  year: number;
+  months: number[];
+};
+
 type EntryDocument = Entry & Document;
 
-export type { UserPayload, AuthRequest, EntryDocument };
+export type { UserPayload, AuthRequest, EntryDocument, EntriesArchive };
