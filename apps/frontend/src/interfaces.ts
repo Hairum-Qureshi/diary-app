7299ef99b67f5da8032e706e089c1f8370cf12a5
Hairum Qureshi <hqureshi@udel.edu>
2026-02-11 11:49:14 -0500

    added 'UseCalendarHook' interface

--- apps/frontend/src/interfaces.ts
+++ apps/frontend/src/interfaces.ts
@@ -22,4 +22,29 @@ interface Entry {
 	createdAt: string;
 }
 
-export type { UseGoogleAuthHook, CalendarDayProps, Entry };
+interface UseCalendarHook {
+	currentMonth: string;
+	currentYear: number;
+	getDaysInMonth: (month: string, year: number) => number;
+	getNextMonth: () => void;
+	getPreviousMonth: () => void;
+	getWeekDayOfMonth: (month: string, year: number, day: number) => string;
+	isToday: (month: string, year: number, day: number) => boolean;
+	months: readonly string[];
+	monthEntriesData: [
+		{
+			_id: string;
+			title: string;
+			createdAt: string;
+		}
+	];
+	allEntries: {
+		postedYears: number[];
+		archives: {
+			year: number;
+			months: number[];
+		}[];
+	};
+}
+
+export type { UseGoogleAuthHook, CalendarDayProps, Entry, UseCalendarHook };
