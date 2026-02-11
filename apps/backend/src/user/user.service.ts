3a50857fed9b49b75489bce3147ceb93b368e30d
Hairum Qureshi <hqureshi@udel.edu>
2026-02-11 14:29:17 -0500

    created method for deleting the user's account and all data associated with it

---
+++ apps/backend/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/User';
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @Inject('FIREBASE_ADMIN') private firebase: admin.app.App,
+  ) {}
+
+  async deleteUserData(uid: string) {
+    // Delete all entries associated with the user
+    await this.userModel.deleteMany({ uid: uid });
+
+    // delete the user account itself
+    await this.userModel.deleteOne({ _id: uid });
+
+    // delete the user's email from Firebase too
+    await this.firebase.auth().deleteUser(uid);
+  }
+}
