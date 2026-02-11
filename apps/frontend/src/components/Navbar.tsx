a16c2d20879626739e63265ddd8026139aaffb39
Hairum Qureshi <hqureshi@udel.edu>
2026-02-11 00:50:22 -0500

    added 'archive' route to Navbar

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,23 +1,38 @@
 export default function Navbar() {
-  return (
-    <div className = "bg-zinc-950 pt-6 text-white text-center">
-        <ul>
-            <li className="inline-block mx-4">
-                <a href="/" className="hover:text-emerald-500 transition">Home</a>
-            </li>
-            <li className="inline-block mx-4">
-                <a href="/about" className="hover:text-emerald-500 transition">About</a>
-            </li>
-            <li className="inline-block mx-4">
-                <a href="/dashboard" className="hover:text-emerald-500 transition">Dashboard</a>
-            </li>
-            <li className="inline-block mx-4">
-                <a href="/calendar" className="hover:text-emerald-500 transition">Calendar</a>
-            </li>
-            <li className="inline-block mx-4">
-                <a href="/new-entry" className="hover:text-emerald-500 transition">New Entry</a>
-            </li>
-        </ul>
-    </div>
-  )
+	return (
+		<div className="bg-zinc-950 pt-6 text-white text-center">
+			<ul>
+				<li className="inline-block mx-4">
+					<a href="/" className="hover:text-emerald-500 transition">
+						Home
+					</a>
+				</li>
+				<li className="inline-block mx-4">
+					<a href="/about" className="hover:text-emerald-500 transition">
+						About
+					</a>
+				</li>
+				<li className="inline-block mx-4">
+					<a href="/dashboard" className="hover:text-emerald-500 transition">
+						Dashboard
+					</a>
+				</li>
+				<li className="inline-block mx-4">
+					<a href="/calendar" className="hover:text-emerald-500 transition">
+						Calendar
+					</a>
+				</li>
+				<li className="inline-block mx-4">
+					<a href="/new-entry" className="hover:text-emerald-500 transition">
+						New Entry
+					</a>
+				</li>
+				<li className="inline-block mx-4">
+					<a href="/all-entries" className="hover:text-emerald-500 transition">
+						Archive
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
