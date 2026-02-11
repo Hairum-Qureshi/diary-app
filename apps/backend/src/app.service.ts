a1382386a0d92a34fd2ac76c07309006e53284c3
Hairum Qureshi <hqureshi@udel.edu>
2026-02-11 16:21:03 -0500

    removed 'getHello' method and replaced it with a server status object

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  serverStats() {
+    return {
+      message: 'Server is running smoothly',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+    };
   }
 }
