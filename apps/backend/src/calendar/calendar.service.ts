f0af6bbc3b1f9b0682168a67e72f720a0137459f
Hairum Qureshi <hqureshi@udel.edu>
2026-02-10 23:38:46 -0500

    wrote method for handling database query logic for getting all written entries for the month

--- apps/backend/src/calendar/calendar.service.ts
+++ apps/backend/src/calendar/calendar.service.ts
@@ -8,4 +8,15 @@ export class CalendarService {
   constructor(
     @InjectModel(Entry.name) private entryModel: Model<EntryDocument>,
   ) {}
+
+  async getEntriesByMonth(month: string, year: string, userId: string) {
+    const startDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+    const endDate = new Date(parseInt(year), parseInt(month), 0);
+    return await this.entryModel
+      .find({
+        uid: userId,
+        createdAt: { $gte: startDate, $lte: endDate },
+      })
+      .select('title');
+  }
 }
