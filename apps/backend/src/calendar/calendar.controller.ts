110a86e1d3f717a9174953fb2ab5a25037f5c968
Hairum Qureshi <hqureshi@udel.edu>
2026-02-10 23:38:21 -0500

    created GET endpoint for getting all entries by month

--- apps/backend/src/calendar/calendar.controller.ts
+++ apps/backend/src/calendar/calendar.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { CalendarService } from './calendar.service';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import * as types from 'src/types';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('calendar')
 export class CalendarController {
   constructor(private calendarService: CalendarService) {}
+
+  @Get('entries/:month/:year')
+  @UseGuards(AuthGuard())
+  async getEntriesByMonth(
+    @Param('month') month: string,
+    @Param('year') year: string,
+    @CurrentUser() user: types.UserPayload,
+  ) {
+    return await this.calendarService.getEntriesByMonth(month, year, user._id);
+  }
 }
