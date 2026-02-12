0c65523a4c8e36d8f3e3a0ff0bec76a25df01d4f
Hairum Qureshi <hqureshi@udel.edu>
2026-02-12 00:00:23 -0500

    wrapped auth-only routes with a custom ProtectedRoutesGuard component

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -9,6 +9,7 @@ import DiaryEntry from "../pages/DiaryEntry";
 import Archive from "../pages/Archive";
 import Profile from "../pages/Profile";
 import "../css/index.css";
+import ProtectedRoutesGuard from "./middleware/ProtectedRoutesGuard";
 
 export default function App() {
 	// TODO - wrap certain routes with a middleware auth component
@@ -18,16 +19,30 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<Landing />} />
 				<Route path="/about" element={<About />} />
-				<Route path="/calendar" element={<Calendar />} />
-				<Route path="/profile" element={<Profile />} />
-				<Route
-					path="/calendar?month=:month&year=:year"
-					element={<Calendar />}
-				/>
-				<Route path="/new-entry" element={<DiaryForm />} />
-				<Route path="/all-entries" element={<Archive />} />
-				<Route path="/entry/:month/:day/:year/edit" element={<DiaryForm />} />
-				<Route path="/entry/:month/:day/:year" element={<DiaryEntry />} />
+				<ProtectedRoutesGuard>
+					<Route path="/calendar" element={<Calendar />} />
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route path="/profile" element={<Profile />} />
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route
+						path="/calendar?month=:month&year=:year"
+						element={<Calendar />}
+					/>
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route path="/new-entry" element={<DiaryForm />} />
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route path="/all-entries" element={<Archive />} />
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route path="/entry/:month/:day/:year/edit" element={<DiaryForm />} />
+				</ProtectedRoutesGuard>
+				<ProtectedRoutesGuard>
+					<Route path="/entry/:month/:day/:year" element={<DiaryEntry />} />
+				</ProtectedRoutesGuard>
 				<Route path="/entry/:entryID?shareable=true" element={<DiaryEntry />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
